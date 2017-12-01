fe5bcfde99b95d17a04813addeefccbe21127fe4
Denis Vostrikov <vostrikov@mindbox.ru>
2017-12-01 14:09:35 +0300

    Добавлен метод tre для обработки ситуации когда может быть передан в качестве параметра react-компонент или строка

--- index.tsx
+++ index.tsx
@@ -8,8 +8,8 @@ declare global {
 
 /**
  * Method for translating react-components
- * @param label String which need to translate
- * @param parameters If translating string include any template, I have to pass object with value, read more https://help.localizejs.com/docs/library-api#section-localize-translate
+ * @param label String which needs to translate
+ * @param parameters If translating string include any template, You have to pass object with value, read more https://help.localizejs.com/docs/library-api#section-localize-translate
  * @param spanClassName Use for passing additional classes
  */
 export function tr(label: string, parameters: object = null, spanClassName: string = null): JSX.Element {
@@ -23,9 +23,21 @@ export function tr(label: string, parameters: object = null, spanClassName: stri
 
 /**
  * Method for translating only string
- * @param label String which need to translate
- * @param parameters If translating string include any template, I have to pass object with value, read more https://help.localizejs.com/docs/library-api#section-localize-translate
+ * @param label String which needs to translate
+ * @param parameters If translating string include any template, You have to pass object with value, read more https://help.localizejs.com/docs/library-api#section-localize-translate
  */
 export function trl(label: string, parameters: object = null): string {
 	return window.tr(label, parameters).replace(/(<\/?[\w\s='":]+\/?>)/g, "");
+}
+
+/**
+ * Method for translating only string or react-component
+ * @param label String which needs to translate
+ * @param parameters If translating string include any template, You have to pass object with value, read more https://help.localizejs.com/docs/library-api#section-localize-translate
+ */
+export function tre(label: string | JSX.Element, parameters: object = null): string | JSX.Element {
+	if (typeof label === "string")
+		return trl(label, parameters);
+
+	return label;
 }
